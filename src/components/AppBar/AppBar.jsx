b297e7445af8a71a5e9fff11c595d08260bd9d08
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-08-24 16:37:37 +0200

    updated css files

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -8,21 +8,25 @@ export const AppBar = () => {
     <header className={s.header}>
       <nav>
         <ul className={s.list}>
-          <li>
-            <NavLink className={addActive} to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className={addActive} to="register">
-              Register
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className={addActive} to="login">
-              Log In
-            </NavLink>
-          </li>
+          <div>
+            <li>
+              <NavLink className={addActive} to="/">
+                Home
+              </NavLink>
+            </li>
+          </div>
+          <div className={s.span}>
+            <li>
+              <NavLink className={addActive} to="register">
+                Register
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={addActive} to="login">
+                Log In
+              </NavLink>
+            </li>
+          </div>
         </ul>
       </nav>
     </header>
