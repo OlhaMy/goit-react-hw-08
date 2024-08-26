610e100ecde24bb3cdfe4755a5e47ead1af4d5e3
Olha Mykhalchuk <124881449+OlhaMy@users.noreply.github.com>
2024-08-26 11:59:34 +0200

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,11 +1,14 @@
 import { LoginForm } from "components";
-import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../redux/auth/selectors";
-import { Navigate } from "react-router-dom";
 
 const LoginPage = () => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  if (isLoggedIn) return <Navigate to="/contacts" />;
-  return <LoginForm />;
+  const handleSubmit = (values) => {
+    console.log("Form submitted with values:", values);
+  };
+
+  return (
+    <div>
+      <LoginForm handleSubmit={handleSubmit} />
+    </div>
+  );
 };
 export default LoginPage;
